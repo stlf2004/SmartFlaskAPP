80738f01a929e96baa8d023abb5bc62c7ec47947
Johnny Loo <johnnyloo1985@gmail.com>
2023-10-02 15:45:12 +0800

    update jenkinsscript.sh

--- jenkinsscript.sh
+++ jenkinsscript.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 . ~/.bashrc
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+if command -v pyenv 1>/dev/null 2>&1; then
+ eval "$(pyenv init --path)"
+fi
+
 pyenv versions
 
 pyenv global 3.10.0
